2c104b8b195fc4c2904fa3bab95b01a92d1a2cd1
Ryan Ravikumar <ryanadrianravikumar@gmail.com>
2025-08-28 23:35:16 +0530

    added entities for user & route microservices with required attributes and constructors

---
+++ microservices/route/src/main/java/com/tursa/route/entity/Edge.java
@@ -0,0 +1,38 @@
+package com.tursa.route.entity;
+
+public class Edge {
+    private String from;
+    private String to;
+    private double distance;
+
+    public Edge(String from, String to, double distance) {
+        this.from = from;
+        this.to = to;
+        this.distance = distance;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+}
+
