c5aa17b3c94e9a38743672c33ee239ce9c5812a8
Ryan Ravikumar <ryanadrianravikumar@gmail.com>
2025-08-31 17:06:27 +0530

    updated firebase config file

--- microservices/shelter/src/main/java/com/tursa/shelter/config/FirebaseConfig.java
+++ microservices/shelter/src/main/java/com/tursa/shelter/config/FirebaseConfig.java
@@ -3,27 +3,31 @@ package com.tursa.shelter.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
-import java.io.IOException;
 
 @Configuration
 public class FirebaseConfig {
-
     @PostConstruct
-    public void initFirebase() throws IOException {
-        FileInputStream serviceAccount =
-                new FileInputStream("src/main/resources/firebase-service-account.json");
+    public void initFirebase() {
+        try {
+            FileInputStream serviceAccount =
+                    new FileInputStream("src/main/resources/smart-evacuation-system-firebase-adminsdk-fbsvc-c2b2b3872d.json");
 
-        FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .setDatabaseUrl("https://<your-database-name>.firebaseio.com/")
-                .build();
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setDatabaseUrl("https://smart-evacuation-system-default-rtdb.asia-southeast1.firebasedatabase.app/") // replace with your Firebase DB URL
+                    .build();
 
-        if (FirebaseApp.getApps().isEmpty()) {
-            FirebaseApp.initializeApp(options);
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
+
+            System.out.println("Firebase initialized");
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to initialize Firebase", e);
         }
     }
 }
