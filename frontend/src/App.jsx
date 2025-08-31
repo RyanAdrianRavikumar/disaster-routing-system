cd49818e917c54cf7ffb40b7a0104073e593da51
Semal Fernando <fernandosemal2124@gmail.com>
2025-08-31 23:37:22 +0530

    Route UI done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,16 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import RescueQueueUI from './pages/RescueQueueUI';
 import ShelterManagementUI from './pages/ShelterManagementUI';
+import RouteManagementUI from './pages/RouteManagementUI';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/r" element={<RescueQueueUI />} />
-          <Route path="/s" element={<ShelterManagementUI />} />
+          <Route path="/rescuequeue" element={<RescueQueueUI />} />
+          <Route path="/sheltermanagement" element={<ShelterManagementUI />} />
+          <Route path="/routemanagement" element={<RouteManagementUI />} />
           {/* Add other routes as needed */}
         </Routes>
       </div>
