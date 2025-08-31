358c0129884fbaf5dc47466c9566e3e148eca959
Umar Fareed <mufareed2002@gmail.com>
2025-08-31 16:41:51 +0530

    updates shelter service and added firebase config file

---
+++ microservices/shelter/src/main/java/com/tursa/shelter/config/FirebaseConfig.java
@@ -0,0 +1,29 @@
+package com.tursa.shelter.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @PostConstruct
+    public void initFirebase() throws IOException {
+        FileInputStream serviceAccount =
+                new FileInputStream("src/main/resources/firebase-service-account.json");
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setDatabaseUrl("https://<your-database-name>.firebaseio.com/")
+                .build();
+
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.initializeApp(options);
+        }
+    }
+}
