3f9028db45a05d823f5dccf14fd391bce05f6109
Umar Fareed <mufareed2002@gmail.com>
2025-09-21 14:49:04 +0530

    added firebase config

---
+++ microservices/shelterroute/src/main/java/com/tursa/shelterroute/config/FirebaseConfig.java
@@ -0,0 +1,33 @@
+package com.tursa.shelterroute.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+
+@Configuration
+public class FirebaseConfig {
+    @PostConstruct
+    public void initFirebase() {
+        try {
+            FileInputStream serviceAccount =
+                    new FileInputStream("src/main/resources/disasterhub-fbe9c-firebase-adminsdk-fbsvc-ad7e354428.json");
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setDatabaseUrl("https://disasterhub-fbe9c-default-rtdb.asia-southeast1.firebasedatabase.app/") // replace with your Firebase DB URL
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
+
+            System.out.println("Firebase initialized");
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to initialize Firebase", e);
+        }
+    }
+}
